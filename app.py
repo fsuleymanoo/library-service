8aa72f3fa8300eb95971f91b85a2eda1ac94d4fd
Faig Suleymanov <faiqsuleymanoo@gmail.com>
2025-04-06 23:46:09 -0400

    code refactor

--- app.py
+++ app.py
@@ -1,91 +1,33 @@
 from flask import Flask, jsonify, request, render_template
-
-app = Flask(__name__)
-
-
-books = [
-    {"book_id": "001", "title": "JAVA", "author": "John Doe", "publication_year": 2000,
-        "genre": "coding", "read_status": "read", "rating": 3.5, "notes": "do not read again"},
-    {"book_id": "002", "title": "JS", "author": "Mark", "publication_year": 2004,
-     "genre": "coding", "read_status": "reading", "rating": 4.5, "notes": "read again"}
-]
-
-stats = []
+from controllers.book_controller import book_bp
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(book_bp)
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"error": "Bad Request"}), 400
+    
+    @app.errorhandler(404)
+    def bad_request(error):
+        return jsonify({"error": "Not Found"}), 404
+    
+    @app.errorhandler(500)
+    def bad_request(error):
+        return jsonify({"error": "Internal Service Error"}), 500
+
+    return app
 
 
-@app.route("/api/books/", methods=["GET"])
-def get_books():
-    return jsonify(books), 200
 
 
-@app.route("/api/books/<string:book_id>/", methods=["GET"])
-def get_single_book(book_id):
-    for book in books:
-        if book_id == book.get("book_id"):
-            return jsonify(book), 200
-    return jsonify({"error": f"Book not found with book_id {book_id}"}), 404
 
 
-@app.route("/api/books", methods=["POST"])
-def add_book():
-    new_book = request.json
-    if not new_book:
-        return jsonify({"error": "The payload cannot be empty"}), 400
-    if "book_id" not in new_book or "title" not in new_book or "author" not in new_book:
-        return jsonify({"error": "Missing required fields (book_id, title, author)"}), 400
-    rating = float(new_book["rating"])
-    if rating < 0 or rating > 5:
-        return jsonify({"error": "rating range 0-5"}), 400
-    valid_status = ["read", "reading", "to-read"]
-    if new_book["read_status"] not in valid_status:
-        return jsonify({"error": "valid read_status (read, reading, to-read)"}), 400
-    for book in books:
-        if new_book.get("book_id") == book.get("book_id"):
-            return jsonify({"error": f"book_id is already present"}), 400
-    books.append(new_book)
-    return jsonify({"status": "Book successfully added", "books": books}), 201
 
-
-@app.route("/api/books/<string:book_id>", methods=["PUT"])
-def update_book(book_id):
-    book_update = None
-    for book in books:
-        if book_id == book.get("book_id"):
-            book_update = book
-    if book_update:
-        new_book = request.json
-        book_update.update(new_book)
-        return jsonify({"status": "Book successfully updated", "books": books}), 200
-
-    return jsonify({"error": f"Book not found with book_id {book_id}"}), 404
-
-
-@app.route("/api/books/<string:book_id>", methods=["DELETE"])
-def delete_book(book_id):
-    book_delete = None
-    for book in books:
-        if book_id == book.get("book_id"):
-            book_delete = book
-    if book_delete:
-        books.remove(book_delete)
-        return jsonify({"status": "Book successfully deleted", "books": books}), 200
-
-    return jsonify({"error": f"Book not found with book_id {book_id}"}), 404
-
-
-@app.route("/api/books/stats", methods=["GET"])
-def get_stats():
-    ratings = [float(book.get("rating", 0))
-               for book in books if "rating" in book]
-    average_rating = round(sum(ratings) / len(ratings), 2) if ratings else 0.0
-    read_books = [book for book in books if book.get("read_status") == "read"]
-    return jsonify({"total_books": len(books), "books_read": len(read_books), "average_rating": average_rating}), 200
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
---
+++ services/book_service.py
@@ -0,0 +1,79 @@
+from db import book_data
+from models.book_model import Book
+
+
+class BookService:
+
+    @staticmethod
+    def get_all_books():
+        return book_data
+
+    @staticmethod
+    def get_book_by_book_id(book_id):
+        for book in book_data:
+            if book.book_id == book_id:
+                return book
+        return None
+
+    @staticmethod
+    def create_book(data: dict):
+
+        if not data.get("book_id"):
+            return {"book_id": "book_id is required"}
+
+        if not data.get("author"):
+            return {"author": "author is required"}
+
+        if not data.get("title"):
+            return {"title": "title is required"}
+
+        if not isinstance(data.get("book_id"), str):
+            return {"book_id": "book_id should be a string"}
+
+        if not isinstance(data.get("title"), str):
+            return {"title": "title should be a string"}
+
+        if not isinstance(data.get("author"), str):
+            return {"author": "author should be a string"}
+
+        if not isinstance(data.get("rating"), float):
+            return {"rating": "rating should be a float"}
+
+        if not isinstance(data.get("publication_year"), int):
+            return {"publication_year": "publication_year should be a intiger"}
+
+        if not data.get("book_id").startswith("B"):
+            return {"book_id": f"book_id {data.get("book_id")} should starts with B_"}
+
+        valid_status = ["read", "reading", "to-read"]
+        if data.get("read_status") not in valid_status:
+            return {"read_status": "read_status must be read, reading or to-read"}
+
+        if data.get("rating") < 0 or data.get("rating") > 5:
+            return {"rating": "rating must be float in 0-5"}
+
+        for book in book_data:
+            if book.book_id == data.get("book_id"):
+                return {"book_id": f"book_id {data.get("book_id")} already exists"}
+
+        new_book = Book(
+            book_id=data.get("book_id"),
+            author=data.get("author"),
+            title=data.get("title"),
+            publication_year=data.get("publication_year"),
+            rating=data.get("rating"),
+            read_status=data.get("read_status"),
+            genre=data.get("genre"),
+            notes=data.get("notes")
+        )
+
+        book_data.append(new_book)
+        return book_data
+    
+    @staticmethod
+    def delete_book(book_id):
+        for book in book_data:
+            if book.book_id == book_id:
+                book_data.remove(book)
+                return True, book_data
+        return False, None    
